63df9d4339396f382dfbbe4a6b05d6c03ebd00c1
Poruch <139491864+Poruch@users.noreply.github.com>
2025-02-15 21:57:08 +0500

    start reorganize Array

--- IndividualWorkTOI/Array.h
+++ IndividualWorkTOI/Array.h
@@ -9,50 +9,58 @@ namespace Arrays {
 	struct MyArray {
 	public:
 
-		MyArray() {
+		MyArray();
+		MyArray(int len);
+		~MyArray();
+
+		MyArray GetArrayFromFile(MyTypes::Image** arr, int* len, std::string fileName);
+		
+		void Reconstruct();
+
+		void WriteArray();
+
+		void SortArray(int (*criterion)(RECORD), bool rise = true);
+
+		void GetSortIndexes(int** indexes, int (*criterion)(MyTypes::Image), bool rise = true);
+		void GetSortIndexes(int** indexes, std::string(*criterion)(MyTypes::Image), bool rise = true);
+
+		int FindAll(int** indexes, int* lenIndexes, int value, int (*criterion)(MyTypes::Image));
+		int FindAll(int** indexes, int* lenIndexes, std::string value, std::string(*criterion)(MyTypes::Image));
+
+		int FindBinary(int value, int(*criterion)(RECORD));
+		int FindBinary(int value, int(*criterion)(RECORD), int leftOffset, int rightOffset);
+		int FindBinary(std::string value, std::string(*criterion)(MyTypes::Image));
+		int FindBinary(std::string value, std::string(*criterion)(MyTypes::Image), int leftOffset, int rightOffset);
+
+		int Find(MyTypes::Image value);
+		int Find(int value, int(*criterion)(MyTypes::Image));
+		int Find(std::string value, std::string(*criterion)(MyTypes::Image));
+
+		void DeleteElems(int value, int(*criterion)(MyTypes::Image));
+		void DeleteElems(std::string value, std::string(*criterion)(MyTypes::Image));
+
+		void DeleteFirstElem(int value, int(*criterion)(MyTypes::Image));
+		void DeleteFirstElem(std::string value, std::string(*criterion)(MyTypes::Image));
+		void DeleteFirstElem(MyTypes::Image value);
+
+		void AddElem(MyTypes::Image value);
+		void AddElems(int count);
+		void AddElems(std::string fileName);
+
+		void RecoverIndexes(bool rise = true);
+
+		RECORD operator[](int index) {
 
 		}
-		void static GetArrayFromFile(MyTypes::Image** arr, int* len, std::string fileName);
-		void GetArray(RECORD** arr, int len);
-		void Reconstruct(RECORD* arr, int len);
 
-		void WriteArray(RECORD* arr, int len);
-
-		void SortArray(RECORD* arr, int len, int (*criterion)(RECORD), bool rise = true);
-
-		void GetSortIndexes(RECORD* arr, int** indexes, int len, int (*criterion)(MyTypes::Image), bool rise = true);
-		void GetSortIndexes(RECORD* arr, int** indexes, int len, std::string(*criterion)(MyTypes::Image), bool rise = true);
-
-		int FindAll(RECORD* arr, int** indexes, int* lenIndexes, int len, int value, int (*criterion)(MyTypes::Image));
-		int FindAll(RECORD* arr, int** indexes, int* lenIndexes, int len, std::string value, std::string(*criterion)(MyTypes::Image));
-
-		int FindBinary(RECORD* arr, int len, int value, int(*criterion)(RECORD));
-		int FindBinary(RECORD* arr, int len, int value, int(*criterion)(RECORD), int leftOffset, int rightOffset);
-		int FindBinary(RECORD* arr, int len, std::string value, std::string(*criterion)(MyTypes::Image));
-		int FindBinary(RECORD* arr, int len, std::string value, std::string(*criterion)(MyTypes::Image), int leftOffset, int rightOffset);
-
-		int Find(MyTypes::Image* arr, int len, MyTypes::Image value);
-		int Find(MyTypes::Image* arr, int len, int value, int(*criterion)(MyTypes::Image));
-		int Find(MyTypes::Image* arr, int len, std::string value, std::string(*criterion)(MyTypes::Image));
-
-		void DeleteElems(MyTypes::Image** arr, int* len, int value, int(*criterion)(MyTypes::Image));
-		void DeleteElems(MyTypes::Image** arr, int* len, std::string value, std::string(*criterion)(MyTypes::Image));
-
-		void DeleteFirstElem(MyTypes::Image** arr, int* len, int value, int(*criterion)(MyTypes::Image));
-		void DeleteFirstElem(MyTypes::Image** arr, int* len, std::string value, std::string(*criterion)(MyTypes::Image));
-		void DeleteFirstElem(MyTypes::Image** arr, int* len, MyTypes::Image value);
-
-		void AddElem(MyTypes::Image** arr, int* len, MyTypes::Image value);
-		void AddElems(MyTypes::Image** arr, int* len, int count);
-		void AddElems(MyTypes::Image** arr, int* len, std::string fileName);
-
-		void RecoverIndexes(MyTypes::Image* arr, int len, bool rise = true);
+		int Count() const {
+			return length;
+		};
 	private:
-		RECORD records;
+		RECORD* records;
+		size_t length;
+
 
 		void ReSize();
 	};
-	
-
-
 }
